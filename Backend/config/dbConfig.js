226da8b17778dca0dac8d3e2d97ede77f2a4685e
Hitanshu <galahitanshu@gmail.com>
2025-06-14 21:48:51 +0530

    deployed

--- Backend/config/dbConfig.js
+++ Backend/config/dbConfig.js
@@ -6,8 +6,11 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const connectionString = process.env.POSTGRES_URI;
 
-const pool= new Pool({
-    connectionString: isProduction? process.env.DATABASE_URL:connectionString
-})
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false // This is needed for Render's PostgreSQL
+    }
+});
 
-module.exports = pool
+module.exports = pool;
